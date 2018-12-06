65a67394156eb09edafe527055fec3c5a42966de
Miles <milesasp@gmail.com>
2018-12-06 11:45:45 +0800

    Lab2_LoginCounter

---
+++ DotNETCore.Lab/Lab2_LoginCounter/Counter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNETCore.Lab.Lab2_LoginCounter
+{
+    public class Counter
+    {
+        private int _count = 0;
+
+        public int CurrentCount
+        {
+            get
+            {
+                return _count;
+            }
+        }
+
+        public void Add(int i)
+        {
+            _count += i;
+        }
+    }
+}
